8be96fdee77c77c22f718c360b42a1fd6bac79df
Bård-Kristian Krohg <baard.krohg@gmail.com>
2017-09-08 22:10:29 +0900

    added a small test for webcam and a CMake file for building

---
+++ webcam.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+  const char* gst = "nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)320, height=(int)240, format=(string)I420, framerate=(fraction)120/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)I420 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+  
+  VideoCapture cap(1);
+  Mat frame;
+
+  if (!cap.isOpened()) {
+    cout << "Could not open webcamera" << endl;
+    return 1;
+  }
+  
+  for (;;) {
+    if (!cap.read(frame)) break;
+
+    imshow("webcam", frame);
+
+    if (waitKey(30) >= 0) break;
+  }
+
+  return 0;
+}

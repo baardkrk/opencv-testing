ac0b91e6135f82669b05a5a808aa3ea21f17f3ab
baardkrk <baard.krohg@gmail.com>
2018-05-25 13:58:49 +0200

    updated architecure

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # opencv-testing
 A collecion of small programs to test the functionality of newly installed opencv systems.
+
+use
+```
+$ mkdir build && cd build
+$ cmake ..
+$ make
+```
+
+TODO:
+make binary files go in `bin` directory
+
--- src/webcam.cpp
+++ src/webcam.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
@@ -8,8 +9,13 @@ int main(int argc, char *argv[])
 {
 
   const char* gst = "nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)320, height=(int)240, format=(string)I420, framerate=(fraction)120/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)I420 ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+
+  int cam_num = 0;
+  if (argc > 1)
+    cam_num = atoi(argv[1]);
+    
   
-  VideoCapture cap(1);
+  VideoCapture cap(cam_num);
   Mat frame;
 
   if (!cap.isOpened()) {
